3be28c7e40e36daf6396510d9a6e0e20b5fbd031
行 <liuxing19921203@outlook.com>
2019-01-02 19:04:00 +0900

    算法学习

---
+++ AlgorithmLearning/AlgorithmLearning/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmLearning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            InsertSort();
+        }
+
+        /// <summary>
+        /// 插入排序
+        /// </summary>
+        static void InsertSort()
+        {
+            //排序数组
+            int[] array = new int[] { 5, 2, 4, 6, 1, 3, 7, 9, 8, 5, 6, 4, 3 };
+            for (var j=0;j<array.Length;j++)
+            {
+                var key = array[j];
+                
+                var i = j - 1;
+                //降序
+                //while (i>=0&&array[i]<key)
+                //升序
+                while (i >= 0 && array[i] > key)
+                {
+                    array[i + 1] = array[i];
+                    i--;
+                }
+                array[i+ 1] = key;
+            }
+
+            foreach (var item in array)
+            {
+
+            Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
